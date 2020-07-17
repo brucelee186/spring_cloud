9e99fb54db60c74231cc6953d61a9e6aff6a4003
admin <brucelee186@163.com>
2020-07-18 01:49:38 +0800

    ribbon is avaiable

---
+++ ribbon/src/main/java/com/framework/RibbonApplication.java
@@ -0,0 +1,21 @@
+package com.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class RibbonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonApplication.class, args);
+    }
+
+    @Bean
+    // 声音一个基于Ribbon的负载均衡服务
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ ribbon/src/main/java/com/framework/controller/RibbonHandler.java
@@ -0,0 +1,29 @@
+package com.framework.controller;
+
+import com.framework.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/ribbon")
+public class RibbonHandler {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/findAll")
+    public Collection<Student> findAll(){
+        // return restTemplate.getForObject("http://localhost:8010/student/findAll")
+         return restTemplate.getForObject("http://eureka-provider/student/findAll", Collection.class);
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return restTemplate.getForObject("http://eureka-provider/student/index", String.class);
+    }
+}
