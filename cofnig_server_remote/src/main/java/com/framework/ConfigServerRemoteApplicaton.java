8d94ed59958ffdfd7c2904aff39f33ccb9089994
admin <brucelee186@163.com>
2020-07-19 17:45:34 +0800

    add config remote server and client model

---
+++ cofnig_server_remote/src/main/java/com/framework/ConfigServerRemoteApplicaton.java
@@ -0,0 +1,13 @@
+package com.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerRemoteApplicaton {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerRemoteApplicaton.class);
+    }
+}
